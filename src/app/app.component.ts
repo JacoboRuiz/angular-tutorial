63f7d917200e3f5872d04c6dba8fa125be185454
Jacobo Ruiz Jimenez <jacoboruiz.malaga@gmail.com>
2025-10-29 17:34:31 +0100

    fix: probando lint-staged con jiti actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [HeaderComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'angular-tutorial';
